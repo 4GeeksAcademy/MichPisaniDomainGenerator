b7fa886a6856c2ba5c6f3bff0b4d0feec4dc2013
MichPisani <michelinapisani@gmail.com>
2025-01-03 18:30:19 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Creando las variables
+  let pronoun = ["our", "my", "your"];
+  let adjective = ["fancy", "silly", "scary"];
+  let noun = ["flower", "monster", "necklace"];
+  let extension = [".com", ".es"];
+  let domains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          domains.push(pronoun[i] + adjective[j] + noun[k] + extension[l]);
+        }
+      }
+    }
+  }
+
+  domains.forEach(element => {
+    console.log(element);
+  });
 };
